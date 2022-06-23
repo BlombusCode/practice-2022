2d4908370fa2f7e697505f2bc33577c1c8cca323
MistahMoose <thomasbradshawbiz@gmail.com>
2022-06-22 22:08:51 -0400

    Implementing PySimpleGui
    implementing py simple, adjusted the print_wage_second function to be implemented with pysimple. Required re-write using time
    
    Blocked out deprecated code for removal - using as reference for now.

--- Wage Display/Wage Display.py
+++ Wage Display/Wage Display.py
@@ -1,11 +1,28 @@
-from os import stat
-import time
+from time import time
+import PySimpleGUI as sg
 
-def wage_per_sec(hourly_wage):#Calculates wage per second
-    secondly_wage = (hourly_wage /3600) # 60 min in hour 60 seconds in min - 60*60 = 3600 |  wage /3600 = wage per second
-    return secondly_wage
+def create_window():
+    sg.theme('black')
+    layout = [
+    [sg.Image('cross.png', pad=0,enable_events = True,key='-CLOSE-')],
+    [sg.Text('Add Minute',font='Franklin 20',key='-MINUTE-'),sg.Text('Add Hour',font='Franklin 20',key='-HOUR-')],
+    [sg.Text('MONEY',font='Franklin 20',key='-TOTAL-')],
+    [sg.Input(key='-INPUT-')],
+    [sg.Button('Start', button_color=('#FFFFFF','#FF0000'),border_width=0,key='-STARTSTOP-')]
+    
+    ]
+    return sg.Window('Money Machine', layout,size=(300,300),no_titlebar = False,element_justification='center')
 
-def print_wage_second(secondly_wage,wage):
+def wage_per_second(wage,elapsed_time):
+    secondly_wage = (wage / 3600)#Converts wage to seconds
+    total = str(round((secondly_wage * elapsed_time),2))#Multiples time and wage per second, rounding to .00
+
+    return ('$',total,'Time', elapsed_time)#Returns as string to update screen
+
+
+
+def deprecated_wage_per_second(wage):
+    secondly_wage = (wage /3600)
     total = 0
     seconds = 40
     minutes = 0
@@ -21,35 +38,53 @@ def print_wage_second(secondly_wage,wage):
         if seconds % 60 == 0:
             minutes += 1
             seconds = seconds - 60
-        print('$', round(total,3), '|',minutes, 'Minute(s)', seconds, "Second(s)" )
-        time.sleep(1.0)
+        return('$', round(total,3), '|',minutes, 'Minute(s)', seconds, "Second(s)" )
 
 
-def add_second():
-    int = 0
-    #empty
 def add_minute():
     int = 0
     #empty function for now
 
+def add_hour():
+    int = 0
+    #empty function for now
 
-running = True
-wage = float(input('Enter hourly wage:'))
+window = create_window()
+active = False
+start_time = 0
+while True:
+    event, values = window.read(timeout=10)
+    
+    if event in(sg.WIN_CLOSED,'-CLOSE-'):
+        break
+    if event == '-STARTSTOP-':
+        if active == False:#Start to active
+            start_time = time()#Update start time
+            active = True
+            window['-STARTSTOP-'].update('Stop')
 
-while running == True:#TODO Async input
+
+    input_value = values['-INPUT-']
+    if input_value.isnumeric():
+        elapsed_time = round(time() - start_time,1)#Difference betwwne times
+        window['-TOTAL-'].update(wage_per_second(int(input_value),elapsed_time))
+
+
+""" wage = float(input('Enter hourly wage:'))
+while True:#TODO Async input
 
     print('Enter 1 to print add a minute \n Enter 2 to add an hour \n Enter 3 at any time to stop the program.')
     print_wage_second(wage_per_sec(wage),wage)
     status = int(input())
     if status == 1:
-        add_second()
-    elif status == 2:
         add_minute()
+    elif status == 2:
+        add_hour()
     elif status == 3:
         running = False
         break
-
-    
+ """
+window.close()
 
 
 #TODO GUI
