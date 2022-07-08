9572248938c619bf74fd09af2c1309af6e3223a5
MistahMoose <thomasbradshawbiz@gmail.com>
2022-07-08 09:59:52 -0400

    Preparing the minutes
    Removed unused functions - Adding functionality to "add minute" feature, Still WIP

--- Wage Display/Wage Display.py
+++ Wage Display/Wage Display.py
@@ -6,11 +6,13 @@ import PySimpleGUI as sg
 def create_window():
     sg.theme('black')
     layout = [
+    [sg.VPush()],
     [sg.Image('cross.png', pad=0,enable_events = True,key='-CLOSE-')],
-    [sg.Text('Add Minute',font='Franklin 20',key='-MINUTE-'),sg.Text('Add Hour',font='Franklin 20',key='-HOUR-')],
+    [sg.Text('Add Minute',font='Franklin 20',key='-ADDMINUTE-', enable_events= True),sg.Text('Add Hour',font='Franklin 20',key='-ADDHOUR-')],
     [sg.Text('$0.00',font='Franklin 20',key='-TOTAL-')],
     [sg.Input(key='-INPUT-')],
-    [sg.Button('Start', button_color=('#FFFFFF','#FF0000'),border_width=0,key='-STARTSTOP-')]
+    [sg.Button('Start', button_color=('#FFFFFF','#FF0000'),border_width=0,key='-STARTSTOP-')],
+    [sg.VPush()]
     
     ]
     return sg.Window('Money Machine', layout,size=(300,300),no_titlebar = False,element_justification='center')
@@ -28,20 +30,16 @@ def wage_per_second(wage,elapsed_time):
     return delimiter.join([str(value) for value in wageupdate])#List comprehension to convert tuple to string and print return.
 
 
-def add_minute():
-    int = 0
-    #empty function for now
-
-def add_hour():
-    int = 0
-    #empty function for now
-
 window = create_window()
 active = False
+elapsed_time = 0
+total_time = 0
 #start_time = 0
 while True:
     event, values = window.read(timeout=10)
     
+#START/STOP PROGRAM
+
     if event in(sg.WIN_CLOSED,'-CLOSE-'):
         break
     if event == '-STARTSTOP-':
@@ -55,11 +53,22 @@ while True:
                 active = False
                 window['-STARTSTOP-'].update('Start')
 
-
+#TIME AND MONEY UPDATES
 
     if active:#Detects if active before updating screen
         input_value = values['-INPUT-']
-        if input_value.isnumeric():#TODO: Refactor so that program does not start until "start" is hit
+        if input_value.isnumeric():
+
+
+            if event == '-ADDMINUTE-':#TODO: Fix functionality
+                print(elapsed_time)
+                #TODO: Does not add to timer, nor money
+                #elapsed_time = total_time + 60 - start_time <-- this should work
+                elapsed_time = round((time() + 60) - start_time, 1)#TODO:Cannot add to time - need to create variable for storing time updates.
+                window['-TOTAL-'].update(wage_per_second(int(input_value), elapsed_time))
+                print(elapsed_time)
+                
+
             elapsed_time = round(time() - start_time,1)#Difference between times
             window['-TOTAL-'].update(wage_per_second(int(input_value),elapsed_time))
 
@@ -67,4 +76,4 @@ window.close()
 
 
 #TODO GUI improvements
-#TODO Stop - Reset - Change Wage
+#TODO Reset - Change Wage
